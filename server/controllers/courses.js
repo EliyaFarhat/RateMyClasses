83f5a2d081104156391a3270000904e954e91ec6
Akshay Vakil <117962555+Octrainn@users.noreply.github.com>
2024-11-05 01:40:42 -0500

    freakbob

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -1,6 +1,10 @@
 // functions to be called by the routes at courses
 //HEY ITS ME FREAKBBOb
 // hi freakbob
+//thanks for answering
+//thanks for answering
+//thanks for answering
+
 export const getCourses = (req, res) => {
     res.send("THIS WORKS!");
 }
