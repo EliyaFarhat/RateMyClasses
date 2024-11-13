2294a7e6e83dce7687de41c737d57764f8e10db5
Akshay Vakil <117962555+Octrainn@users.noreply.github.com>
2024-11-13 18:01:04 -0500

    :3

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -7,7 +7,38 @@
 // YOURE WELCOME FREAKBOB
 //its hard being freeaky 
 // *becomes freaky*
+
+import Course from '../models/courseModel.js';
+
+
+
 export const getCourses = (req, res) => {
     res.send("THIS WORKS!");
 }
  
+
+export const createCourse = async (req, res) => {
+    const { courseName, description, school, courseCode, prerequisites, antirequisites } = req.body;
+
+    const newCourse = new Course({
+        courseName,
+        description,
+        school,
+        courseCode,
+        prerequisites,
+        antirequisites
+    });
+
+    try {
+        const savedCourse = await newCourse.save();
+        res.status(201).json(savedCourse);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+
+export const searchCourses = async(req, res) => {
+    const {query} = req.query 
+}
+
